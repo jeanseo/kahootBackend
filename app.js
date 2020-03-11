9a12733e7c3ec7eae905d4e18a0f631a17a5caba
Jeanseo <jsmpub@gmail.com>
2020-03-11 11:24:58 +0100

    Connection à MongoDB

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const mongoose = require('mongoose');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://kahoot-adm:dKUuASxwrKOUFXlJ@cluster0-hp8cv.mongodb.net/test?retryWrites=true&w=majority',
+    { useNewUrlParser: true,
+        useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use(logger('dev'));
 app.use(express.json());
